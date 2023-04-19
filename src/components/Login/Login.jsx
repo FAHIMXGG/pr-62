c2d670e800330f136b590dc504ce7c05fd35b914
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-04-20 00:36:12 +0600

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../provider/AuthProvider';
 
 
 
-const handleLogin = () =>{
-    
-}
 
 const Login = () => {
+
+    const { signIn } = useContext(AuthContext);
+    const handleLogin = (e) => {
+
+
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset()
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                
+
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
@@ -34,7 +53,7 @@ const Login = () => {
                         </div>
                     </form>
                     <div>
-                        <button  className="btn btn-primary">Google</button>
+                        <button className="btn btn-primary">Google</button>
                     </div>
 
                 </div>
